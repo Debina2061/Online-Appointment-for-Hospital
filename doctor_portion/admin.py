baf1ccedfdc492e405fc7f42b6a60279c4798341
Debina <debinabaraili94@gmail.com>
2025-08-02 10:00:00 +0545

    cleaning up filess

--- doctor_portion/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from doctor_portion.models import DoctorModel, BookingModel
-
-admin.site.register(DoctorModel)
-admin.site.register(BookingModel)
-
-
-# Register your models here.
